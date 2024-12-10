ee32eaeb7c7d549373c7cb0bdb0a214328967d82
Ehsan Ullah <ihsanullahi.r115@gmail.com>
2024-12-09 19:16:27 -0700

    Update arr.js

--- Project/arr.js
+++ Project/arr.js
@@ -63,3 +63,53 @@ document.addEventListener("DOMContentLoaded", () => {
         })
     }
 })
+
+async function getResponceFromLLM() {
+    try {
+        const container = document.querySelector(".llm");
+
+        const inputRef = document.querySelector("input.userQuery");
+        const query = inputRef.value;
+        //  clear input
+        inputRef.value = "";
+
+        const apiBody = {
+            "contents": [
+                {
+                    parts: [
+                        {
+                            text: "You are an accounting-expert consultant , who answer only and only and explain user and also excuses to other queries",
+                        },
+                        { text: query }
+                    ],
+                },
+            ],
+        };
+        const res = await fetch(
+            "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyA2xtnSgMXo3ZKjpTJ919VUI6GkjTSvtqg",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(apiBody),
+            }
+        );
+
+        const data = await res.json();
+        const responseText = data["candidates"][0].content['parts'][0].text;
+
+
+        const responseDiv = document.createElement("div");
+        responseDiv.innerHTML = `
+   <p>${responseText}</p>
+   `;
+        container.appendChild(responseDiv);
+
+
+
+    } catch (err) {
+        console.error(err);
+        alert("Error while generating response");
+    }
+}
